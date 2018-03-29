7d8afaf3d740d0d242eb2b5b64616e13446abe8f
Steve Mahlstedt <steve.mahlstedt@gmail.com>
2018-03-29 12:17:48 -0400

    Submit button takes height width

--- designs.js
+++ designs.js
@@ -5,6 +5,23 @@
 
 function makeGrid() {
 
-// Your code goes here!
-
 }
+
+let submitButton, newHeight, newWidth;
+submitButton = $("#sizePicker").children().last();
+newHeight = $("#inputHeight").attr("min");
+newWidth = $("#inputWidth").attr("min");
+
+$(submitButton).on("click", function(evt){ //waits for submit button click
+	evt.preventDefault();  
+	console.log("Height: " + newHeight);
+	console.log("Width: " + newWidth);
+});
+
+$("#sizePicker").change(function(){ // monitors input text fields and assigns height and width
+	newHeight = $("#inputHeight").val();
+	newWidth = $("#inputWidth").val();
+	console.log("Height or Width changed");
+	console.log(newHeight + " x " + newWidth);
+});
+
